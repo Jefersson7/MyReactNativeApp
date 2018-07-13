cfe86b766ae9f0bcf18f72cdeb6497ae82e6559c
Jefersson Cano <jeferssoncano@seven4n.com>
2018-07-13 16:24:09 -0500

    Added AppCenter SDKs

--- App.js
+++ App.js
@@ -7,22 +7,64 @@
  */
 
 import React, { Component } from 'react';
-import { Platform, StyleSheet, Text, View } from 'react-native';
+import { Platform, StyleSheet, Text, View, Button } from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+import Analytics from 'appcenter-analytics';
+import Crashes from 'appcenter-crashes';
+
+import CodePush from 'react-native-code-push';
 
 export default class App extends Component {
+  sendEvent = () => {
+    Analytics.trackEvent('My Custom Event', {
+      prop1: new Date().getSeconds()
+    });
+  }
+
+  nativeCrash = () => {
+    Crashes.generateTestCrash();
+  }
+
+  func1 = () => { this.func2(); }
+  func2 = () => { this.func3(); }
+  func3 = () => { this.func4(); }
+  func4 = () => { this.func5(); }
+
+  jsCrash = () => {
+    this.func1();
+  }
+
+  func5 = () => { throw new Error("My uncaught JS Exception") }
+
+  constructor(props) {
+    super(props);
+    this.state = { logs: [] };
+  }
+
+  codepushSync = () => {
+    this.setState({logs: ['Started At ' + new Date().getTime()]})
+    CodePush.sync({
+      updateDialog: true,
+      installMode: CodePush.InstallMode.IMMEDIATE
+    }, (status) => {
+      for (var key in CodePush.SyncStatus) {
+        if (status === CodePush.SyncStatus[key]) {
+          this.setState(prevState => ({ logs: [...prevState.logs, key.replace(/_/g, ' ')] }));
+          break;
+        }
+      }
+    });
+  }
+
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        <Button title="Send Event" onPress={() => this.senEvent()} />
+        <Button title="Native Crash" onPress={() => this.nativeCrash()} />
+        <Button title="JS Crash" onPress={() => this.jsCrash()} />
+        <Button title="Code Push" onPress={() => this.codepushSync()} />
+        <Text>{JSON.stringify(this.state.logs)}</Text>
       </View>
     );
   }
